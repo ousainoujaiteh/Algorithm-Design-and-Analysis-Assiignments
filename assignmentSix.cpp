b782bf8e5fa25403c2e9ef45f65c1e9f0d671e67
ousainoujaiteh <ousainoujaiteh@gmail.com>
2022-10-24 18:03:38 +0000

    Assignment 4,5 and 6 Added

---
+++ assignmentSix.cpp
@@ -0,0 +1,63 @@
+//
+// Created by Ousainou Jaiteh on 10/18/22.
+//
+#include <string>;
+#include <unordered_set>;
+#include <iostream>
+
+using namespace std;
+
+class LongestNiceSubstring {
+public:
+    string longestNiceSubstring(string s) {
+
+        pair<int, int> niceSubstring = longestNiceSubstring(s, 0, s.length() - 1);
+        string result = s.substr(niceSubstring.first, niceSubstring.second - niceSubstring.first + 1);
+        if (result.empty()){
+            return "None";
+        }
+        else{
+            return result;
+        }
+    }
+
+    pair<int, int> longestNiceSubstring(string &s, int left, int right) {
+        int splittingIndex = isNotNiceString(s, left, right);
+        pair<int, int> rez = {left, right};
+        if (splittingIndex == -1)return rez;
+        pair<int, int> leftString = longestNiceSubstring(s, left, splittingIndex - 1);
+        pair<int, int> rightString = longestNiceSubstring(s, splittingIndex + 1, right);
+        return (leftString.second - leftString.first >= rightString.second - rightString.first) ? leftString
+                                                                                                : rightString;
+    }
+
+    int isNotNiceString(string &s, int left, int right) {
+        unordered_set<char> set = getCharacterSet(s, left, right);
+        for (int i = left; i <= right; i++) {
+            char ch = s[i];
+            if (set.find(toupper(ch)) == set.end() || set.find(tolower(ch)) == set.end()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    unordered_set<char> getCharacterSet(string &s, int left, int right) {
+        unordered_set<char> charSet;
+        for (int i = left; i <= right; i++) {
+            charSet.insert(s[i]);
+        }
+        return charSet;
+    }
+
+};
+
+
+int main() {
+    string input;
+    cin >> input;
+
+    LongestNiceSubstring *sol = new LongestNiceSubstring();
+    cout << sol->longestNiceSubstring(input);
+    return 0;
+}
--- assignmentThree.cpp
+++ assignmentThree.cpp
@@ -1,40 +1,50 @@
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-bool findTriples(int A[], int size, int sum) {
-    for (int i = 0; i < size - 2; ++i) {
-        for (int j = i + 1; j < size - 1; ++j) {
-            for (int k = j + 1; k < size; ++k) {
-                if (A[i] + A[j] + A[k] == sum) {
-                    cout << "Triplets are: " << A[i] <<
-                         ", " << A[j] << ", " << A[k];
-                    return false;
-                }
+int threeNumberSum(int array[],int size){
+    int count = 0;
+    int targetSum = 0;
+    std::sort(array,array + size);
+//    std::sort(array.begin(),array.end());
+    int left, right, findingSum;
+    vector<vector<int>> res = {};
+    for (int i = 0; i < size - 2; i++) {
+        if (targetSum < array[i]) break;
+        findingSum = targetSum - array[i];
+        left = i +1;
+        right = size -1;
+        while (left < right) {
+            if ((array[left] + array[right]) < findingSum){
+                left ++;
+            } else if ((array[left] + array[right]) > findingSum){
+                right--;
+            } else {
+//                vector<int> triplet = {array[i],array[left],array[right]};
+//                res.push_back(triplet);
+                count++;
+                left++;
+                right--;
             }
         }
     }
-    // cout << "No Triples Found!!! " << endl;
-    return false;
-}
 
+    return count;
+}
 
 int main() {
     int size;
     // cout << "Please Enter The Size of The Array: ";
     cin >> size;
 
-    int arr[size];
+    int array[size] ;//{1,2,-3};
     for (int i = 0; i < size; ++i) {
         // cout << "Enter [" << i << "] Array Element:  ";
-        cin >> arr[i];
+        cin>> array[i];
     }
-
-    int sum;
-    // cout << "Please Enter The Sum of The Triples: ";
-    cin >> sum;
-
-    findTriples(arr, size, sum);
-
+    
+    cout << threeNumberSum(array, size);
+    
     return 0;
 }
